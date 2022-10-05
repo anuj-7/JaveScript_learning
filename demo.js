f26cf9d626d0dc8c455d4d72468778751494797c
ANUJ CHAUHAN <113323751+anuj-7@users.noreply.github.com>
2022-10-05 17:40:30 +0530

    First program execution in JS
    First program execution in JS

---
+++ demo.js
@@ -0,0 +1,21 @@
+
+console.log("My Name is Anuj Kumar Chauhan.");
+console.log("My hobbies are -");
+console.log("1. Bike riding");
+console.log("2. Filming");
+console.log("3. Traveling")
+console.log("")
+console.info("My strength are -");
+console.info("1. Dedication");
+console.info("2. Versatility");
+console.info("3. Flexibility")
+console.log("")
+console.warn("My weakness are -");
+console.warn("1. Competitive");
+console.warn("2. Insecure");
+console.warn("3. Self-critical")
+console.log("")
+console.log("Output of 30 + 50 ="); 
+console.log("30+50")
+console.log("Output of 49*479 ="); 
+console.log("498479")
