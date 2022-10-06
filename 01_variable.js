d9d44752cc04e18a59f1d05303440525ed7a7946
ANUJ CHAUHAN <113323751+anuj-7@users.noreply.github.com>
2022-10-06 20:08:23 +0530

    Variable & Swap Added
    Variable & Swap Added

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+var my_apple // Variable declaration 
+my_apple = "straberry"; // variable initialization
+
+var my_name = "Mohit Sharma"; // Variable declarance and initialization in same line
+my_name = "Mohit Raghav Sharma";
+console.log(my_name);
+console.log(my_apple);
+
+my_apple = "200";
+console.log(my_apple);
---
+++ 01_varriable_assign.js
@@ -0,0 +1,13 @@
+// 1.
+var myFullName; 
+console.log(myFullName);
+myFullName = "Anuj Chauhan";
+console.log(myFullName);
+myFullName = "Anuj Kumar Chauhan"
+console.log(myFullName);
+// 2.
+var myLove = "I love only JS";
+console.log(myLove);
+// 3.
+var firstName="Anuj", lastName="Chauhan", age="28", address="Pune", pin_code="410501", gender="Male", is_married="Married";
+console.log(firstName, lastName, age, address, pin_code, gender, is_married);
