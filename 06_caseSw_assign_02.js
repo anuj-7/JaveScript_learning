545693c8dee299a7ebe4cbb798a8e274a79cbb3d
ANUJ CHAUHAN <113323751+anuj-7@users.noreply.github.com>
2022-10-14 21:15:42 +0530

    Loop & case switch added
    Loop & case switch added

---
+++ 06_caseSw_assign_02.js
@@ -0,0 +1,58 @@
+
+var caseSwitch = function (val) {
+    switch (val) {
+        case 1:
+            console.log(`January`);
+            break;
+            case 2:
+                console.log(`february`);
+                break;
+                case 3:
+            console.log(`March`);
+            break;
+            case 4:
+            console.log(`April`);
+            break;
+            case 5:
+            console.log(`May`);
+            break;
+            case 6:
+            console.log(`June`);
+            break;
+            case 7:
+            console.log(`July`);
+            break;
+            case 8:
+            console.log(`August`);
+            break;
+            case 9:
+            console.log(`September`);
+            break;
+            case 10:
+            console.log(`October`);
+            break;
+            case 11:
+            console.log(`November`);
+            break;
+            case 12:
+            console.log(`December`);
+            break;
+            
+        default:
+            console.log(`Not a month`);
+            break;
+    }
+}
+caseSwitch(1)
+caseSwitch(2)
+caseSwitch(3)
+caseSwitch(4)
+caseSwitch(5)
+caseSwitch(6)
+caseSwitch(7)
+caseSwitch(8)
+caseSwitch(9)
+caseSwitch(10)
+caseSwitch(11)
+caseSwitch(12)
+caseSwitch(13)
---
+++ 06_switchCase.js
@@ -0,0 +1,31 @@
+
+var dayNumber = 2;
+switch (dayNumber) {
+    case 1:
+        console.log(`Monday`);
+        break;
+    case 2:
+        console.log(`Tuesday`);
+        break;
+    case 1:
+        console.log(`Wednesday`);
+        break;
+    case 3:
+        console.log(`Thursday`);
+        break;
+    case 4:
+        console.log(`Friday`);
+        break;
+    case 5:
+        console.log(`Friday`);
+        break;
+    case 6:
+        console.log(`Saturday`);
+        break;
+    case 7:
+        console.log(`Sunday`);
+        break;
+    default:
+        console.log("Invalid input");
+        break;
+}
